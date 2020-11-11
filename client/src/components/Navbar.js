3da96841db79703643aa7e9ef265f7d9b3c54fd1
Raul Laredo Soria <Raul.Laredo@jalasoft.com>
2020-11-10 22:44:14 -0400

    Added logo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Box, Text } from 'gestalt';
+import { Box, Text, Heading, Image } from 'gestalt';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => (
     <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="around"
         height={70}
         color='midnight'
         padding={1}
@@ -11,6 +14,27 @@ const Navbar = () => (
     >
         <NavLink to="/signin">
             <Text size="xl" color="white">Sign in</Text>
+        </NavLink>
+
+        {/* Title and logo */}
+        <NavLink to="/">
+            <Box display="flex" alignItems="center">
+                <Box margin={6} height={50} width={50}>
+                    <Image
+                        alt="Ramalaso Logo"
+                        naturalHeight={1}
+                        naturalWidth={1}
+                        src="./icons/logo.svg"
+                    />
+                </Box>
+                <Heading size="xs" color="orange">
+                    Ramalaso Shoes
+                </Heading>
+            </Box>
+        </NavLink>
+
+        <NavLink to="/signup">
+            <Text size="xl" color="white">Sign up</Text>
         </NavLink>
     </Box>
 );
